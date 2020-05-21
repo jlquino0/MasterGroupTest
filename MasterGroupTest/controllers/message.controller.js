c0409b772e5362937f650284e816f5549a973840
jorge quiñonez <jlquino0@gmail.com>
2020-05-21 00:04:45 -0600

    Authentication

--- MasterGroupTest/controllers/message.controller.js
+++ MasterGroupTest/controllers/message.controller.js
@@ -7,26 +7,50 @@ var messages = {
 }
 
 var nextUniqueId = 0;
+var AlredyKnownKey = 8796;
+var RouteMessage = "/message";
 
 var _ = require("underscore");
 
+var xkey;
+var xroute;
+
 exports.create = function (req, res) {
+
+    xkey = req.header('X-Key');
+    xroute = req.header('X-Route');
+
+    if (xkey != AlredyKnownKey)
+        res.status(403).send();
+
+    if (xroute != RouteMessage)
+        res.status(403).send();
+
     var newmessage = req.body;
     nextUniqueId++;
     newmessage.id = nextUniqueId;
     messages["message" + nextUniqueId] = newmessage;
     res.status(200);
-    res.send(nextUniqueId);
+    res.send(nextUniqueId.toString());
 };
 
 exports.findAll = function (req, res) {
+
     console.log("--->Find All: \n" + JSON.stringify(messages, null, 4));
     res.end("All messages: \n" + JSON.stringify(messages, null, 4));
 };
 
 exports.findAllId = function (req, res) {
-    //var message = messages["message" + req.params.id];
+    xkey = req.header('X-Key');
+    xroute = req.header('X-Route');
 
+    if (xkey != AlredyKnownKey)
+        res.status(403).send();
+
+    if (xroute != RouteMessage)
+        res.status(403).send();
+
+    //var message = messages["message" + req.params.id];
     var filtered = _.where(messages, { id: parseInt(req.params.id) });
 
     res.status(200);
@@ -34,6 +58,14 @@ exports.findAllId = function (req, res) {
 };
 
 exports.findAllTag = function (req, res) {
+    xkey = req.header('X-Key');
+    xroute = req.header('X-Route');
+
+    if (xkey != AlredyKnownKey)
+        res.status(403).send();
+
+    if (xroute != RouteMessage)
+        res.status(403).send();
 
     var filtered = _.where(messages, { tag: req.params.tag });
 
